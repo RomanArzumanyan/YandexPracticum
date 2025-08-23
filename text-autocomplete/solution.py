5dad88597cc46e7434cfc97bf2d4138726ac635e
Roman Arzumanyan <roman.arzum@gmail.com>
2025-08-23 11:01:26 +0300

    download dataset

--- text-autocomplete/solution.py
+++ text-autocomplete/solution.py
@@ -1,13 +1,20 @@
 from src import data_utils, data_set, lstm_model, transformer
-from tqdm import tqdm
+import requests
 
 TOKENIZER = data_set.TOKENIZER
 MIN_LEN = data_set.MIN_LEN
-NUM_TOK = 3
+NUM_WORDS = 3
 ROUGE = lstm_model.ROUGE
 
+
+url = "https://code.s3.yandex.net/deep-learning/tweets.txt"
+filename = "data/raw_dataset.txt"
+response = requests.get(url, stream=True)
+response.raise_for_status()
+
+
 clean = data_utils.clean_up(data_utils.load_dataset(
-    "data/raw_dataset.txt", cap=5000))
+    "data/raw_dataset.txt", cap=10000))
 
 splits = data_utils.split_dataset(clean)
 
@@ -23,18 +30,15 @@ lstm = lstm_model.Lstm(TOKENIZER)
 lstm_model.train(lstm, n_epochs=3, l_rate=0.002, tokenizer=TOKENIZER,
                  train_loader=data_loaders[0], val_loader=data_loaders[1])
 
+
 # Fill inference data.
-# Last NUM_TOK words from every sentence will be trimmed.
-# During the inference, NN will predict NUM_TOK tokens for every sentence.
-# This number is fixed because otherwise (tell NN to predict 1/4 of input) every
-# dataset entry will undergo inference variable amount of times.
 lstm_split = []
 true_split = []
 for i in range(0, len(splits['test'])):
     words = splits['test'][i].split()
-    if len(words) < MIN_LEN + NUM_TOK:
+    if len(words) < MIN_LEN + NUM_WORDS:
         continue
-    ctx_len = len(words) - NUM_TOK
+    ctx_len = len(words) - NUM_WORDS
     context = ' '.join(words[:ctx_len])
     target = ' '.join(words[ctx_len:])
     lstm_split.append(context)
@@ -44,9 +48,9 @@ for i in range(0, len(splits['test'])):
 gpt2_split = lstm_split
 
 # Autoregression is done via concatenation of user input and model output
-# Tell dataset not to trim last token because it's already done above.
-for i in range(0, NUM_TOK):
-    _, dloader = data_set.prepare_data(lstm_split, shuffle=False, num_targets=0)
+for i in range(0, NUM_WORDS):
+    _, dloader = data_set.prepare_data(
+        lstm_split, shuffle=False, num_targets=0)
     preds = lstm_model.inference(lstm, loader=dloader, tokenizer=TOKENIZER)
     lstm_split = [x + ' ' + y for x, y in zip(lstm_split, preds)]
 
@@ -56,7 +60,7 @@ for k, v in rouge_score.items():
     print(f"{k}: {v:.4f}")
 
 gpt2 = transformer.DistilGPT2()
-for i in range(0, NUM_TOK):
+for i in range(0, NUM_WORDS):
     preds = gpt2.inference(gpt2_split)
     gpt2_split = [x + ' ' + y for x, y in zip(gpt2_split, preds)]
 
